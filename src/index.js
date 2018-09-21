1f64004bb636b9a3951ac2a33c5b0b7d6dc7423f
Ewelai <palevo.ewelai@gmail.com>
2018-09-21 20:20:46 +0300

    Ewelai

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+// first part
+var arrSp = equation.split(" ");   //('-20*x^2-108797540*x-130011773690520');
+let a, b, c, d, x1, x2;
+a = arrSp [0];  //-20
+b = (arrSp[3] + arrSp[4]); //-1087...
+c = (arrSp[7] + arrSp[8]); //-1300...
+d = (b*b) - 4*a*c;
+
+// second part
+if (d > 0) {
+  x1 = Math.round((-b+Math.sqrt(d))/(2*a));
+  x2 = Math.round((-b-Math.sqrt(d))/(2*a));
+} else {
+  return null;
 }
+
+// third part
+var arr = [];
+arr.push(x1);
+arr.push(x2);
+
+//fourth part
+return arr.sort(function(a, b) {
+  return a - b;
+})
+};
