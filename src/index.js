a30b9e6d7121358adacec973939360ea838590a1
Ewelai <32734156+Ewelai@users.noreply.github.com>
2018-09-26 13:24:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 module.exports = function solveEquation(equation) {
 // first part
-var arrSp = equation.split(" ");   //('-20*x^2-108797540*x-130011773690520');
+let arrSp = equation.split(" ");   //('-20*x^2-108797540*x-130011773690520');
 let a, b, c, d, x1, x2;
-a = arrSp [0];  //-20
+a = arrSp[0];  //-20
 b = (arrSp[3] + arrSp[4]); //-1087...
 c = (arrSp[7] + arrSp[8]); //-1300...
-d = (b*b) - 4*a*c;
+d = (b * b) - 4 * a * c;
 
 // second part
 if (d > 0) {
-  x1 = Math.round((-b+Math.sqrt(d))/(2*a));
-  x2 = Math.round((-b-Math.sqrt(d))/(2*a));
+  x1 = Math.round( (- b + Math.sqrt(d) ) / (2 * a));
+  x2 = Math.round( (- b - Math.sqrt(d) ) / (2 * a));
 } else {
   return null;
 }
 
 // third part
-var arr = [];
+let arr = [];
 arr.push(x1);
 arr.push(x2);
 
